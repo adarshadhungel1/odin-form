0f421fb93a0541d58e4de069b2ce89784651ae4d
adarshaadhungel1 <dadarsha6@gmail.com>
2023-01-27 21:06:48 +0545

    added a placeholder to check if entered number is number or text

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 let pass = document.querySelector('#pw');
 let repass = document.querySelector('#pwrepeat');
-
 let submitbtn = document.querySelector("button");
+let phonenum = document.querySelector("#number");
+
 submitbtn.addEventListener('click', actions);
+phonenum.addEventListener('onkeydown', numcheck);
 
 function actions(){
     if (pass.value !== repass.value){
         document.querySelector('.pwmismatch').innerHTML = "Password doesn't match!!";
     }
+}
+
+function numcheck(e){
+    console.log(e);
 }
