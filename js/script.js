197de749cdaf50463e5a892d27f07df3c2d1de8a
adarshaadhungel1 <dadarsha6@gmail.com>
2023-02-08 20:08:00 +0545

    cleanup

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 let pass = document.querySelector('#pw');
 let repass = document.querySelector('#pwrepeat');
 let submitbtn = document.querySelector("button");
-let phonenum = document.querySelector("#number");
 
 submitbtn.addEventListener('click', actions);
 
 function actions(){
     if (pass.value !== repass.value){
-        document.querySelector('.pwmismatch').innerHTML = "Password doesn't match!!";
+        document.querySelector('.pwmismatch').innerHTML = "*Password doesn't match!!";
     }
     else {
         document.querySelector('.pwmismatch').innerHTML = "";
