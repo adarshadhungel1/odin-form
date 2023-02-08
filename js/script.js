bbdde5a4d01c508f79ec0fd602d89d34feca3ec8
adarshaadhungel1 <dadarsha6@gmail.com>
2023-02-08 19:40:03 +0545

    added provision for removing pw mismatch text once correct pw is entered

--- js/script.js
+++ js/script.js
@@ -9,4 +9,7 @@ function actions(){
     if (pass.value !== repass.value){
         document.querySelector('.pwmismatch').innerHTML = "Password doesn't match!!";
     }
+    else {
+        document.querySelector('.pwmismatch').innerHTML = "";
+    }
 }
