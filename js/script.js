1fb47b7769d86769076221d7456d403441d60d8c
adarshaadhungel1 <dadarsha6@gmail.com>
2023-02-08 20:19:05 +0545

    added dyanmic color changing of border on password field on incorrect password

--- js/script.js
+++ js/script.js
@@ -4,9 +4,13 @@ let repass = document.querySelector('#pwrepeat');
 function checkpass(e){
     if (pass.value !== repass.value){
         document.querySelector('.pwmismatch').innerHTML = "*Password doesn't match!!";
+        document.querySelector('#pw').style.borderColor = 'red';
+        document.querySelector('#pwrepeat').style.borderColor = 'red';
     }
     else {
         document.querySelector('.pwmismatch').innerHTML = "";
+        document.querySelector('#pw').style.borderColor = '#E5E7EB';
+        document.querySelector('#pwrepeat').style.borderColor = '#E5E7EB';
     }
 }
 
