91c8368b6f507684b2420d6f81f00d802e022b56
adarshaadhungel1 <dadarsha6@gmail.com>
2023-02-08 20:13:00 +0545

    added realtime password matching logic

--- js/script.js
+++ js/script.js
@@ -12,3 +12,15 @@ function actions(){
         document.querySelector('.pwmismatch').innerHTML = "";
     }
 }
+
+function checkpass(e){
+    if (pass.value !== repass.value){
+        document.querySelector('.pwmismatch').innerHTML = "*Password doesn't match!!";
+    }
+    else {
+        document.querySelector('.pwmismatch').innerHTML = "";
+    }
+}
+
+pass.addEventListener('input', checkpass);
+repass.addEventListener('input', checkpass);
